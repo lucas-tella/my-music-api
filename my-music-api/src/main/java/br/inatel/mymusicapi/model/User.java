cfd40cc256c67443430c9ab34bba6244e4c5f41e
Lucas Tellaroli <lucas.tellaroli@inatel.br>
2022-01-06 18:03:07 -0300

    configurando db

--- my-music-api/src/main/java/br/inatel/mymusicapi/model/User.java
+++ my-music-api/src/main/java/br/inatel/mymusicapi/model/User.java
@@ -9,9 +9,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -43,8 +41,4 @@ public class User{
 	
 	@Column(name = "password", nullable = false)
 	private String password;
-	
-//	@OneToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "id_profile", referencedColumnName = "id")
-//	private Profile profile;
 }
